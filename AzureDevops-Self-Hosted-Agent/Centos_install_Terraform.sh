1bd6197e6bb16cbac6be0bd023220fc63b2d2d22
Katef-Cloud <eng.karim.atef@gmail.com>
2022-10-08 00:16:06 +0200

    done

--- AzureDevops-Self-Hosted-Agent/Centos_install_Terraform.sh
+++ AzureDevops-Self-Hosted-Agent/Centos_install_Terraform.sh
@@ -2,6 +2,6 @@
 
 # install Terraform on Centos 7
 
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
-sudo yum -y install terraform && \
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
 terraform -install-autocomplete
